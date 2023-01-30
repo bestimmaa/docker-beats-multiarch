cbba8e0780f45139d5b1c6c00154d082da74239b
Christoph Halang <bestimmaa@users.noreply.github.com>
2023-01-30 15:10:20 +0100

    Removed potential(?) syntax error in bash

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,9 +8,9 @@ if [[ -z $1 ]] || [[ ${1:0:1} == '-' ]] ; then
   exec /usr/share/filebeat/filebeat "$@"
 else
   # They may be looking for a Beat subcommand, like "filebeat setup".    
-  subcommands=$(/usr/share/filebeat/filebeat help \
-                  | awk 'BEGIN {RS=""; FS="\n"} /Available Commands:/' \ 
-                  | awk '/^\s+/ {print $1}')
+  subcommands=$(/usr/share/filebeat/filebeat help |
+  awk 'BEGIN {RS=""; FS="\n"} /Available Commands:/' |
+  awk '/^\s+/ {print $1}')
 
   # If we _did_ get a subcommand, pass it to filebeat.
   for subcommand in $subcommands; do
